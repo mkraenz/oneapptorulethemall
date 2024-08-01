35428fb49f3284faf5a60277cd134af75e40a43a
Mirco Kraenz <software@kraenz.eu>
2024-08-01 12:16:19 +0200

    fix(#1): zuplo dev portal needs at least one route

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiExtension, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiExtension,
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 const ApiZuploRoute = (extensionProperties: object) =>
   ApiExtension('x-zuplo-route', extensionProperties);
 const ApiZuploInternalDocs = () => ApiExtension('x-internal', true);
 
+class GetHelloDto {
+  @ApiProperty({ description: 'Hello message' })
+  hello: string;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -32,5 +41,28 @@ export class AppController {
   @ApiExtension('x-internal', true)
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('hello')
+  @ApiOperation({
+    summary: 'Say hi',
+    description: 'This is a simple endpoint that returns a greeting message.',
+  })
+  @ApiResponse({ type: GetHelloDto, description: 'Greeting message' })
+  @ApiExtension('x-zuplo-route', {
+    corsPolicy: 'none',
+    handler: {
+      export: 'urlForwardHandler',
+      module: '$import(@zuplo/runtime)',
+      options: {
+        baseUrl: process.env.BASE_URL,
+      },
+    },
+    policies: {
+      inbound: ['set-headers-inbound'],
+    },
+  })
+  getHello() {
+    return { hello: 'world' };
   }
 }
